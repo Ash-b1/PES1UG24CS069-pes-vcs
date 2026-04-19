3d0ef893349d1827a00b73feee58a7405c19aa07
vboxuser <vboxuser@unit-4.myguest.virtualbox.org>
2026-04-19 13:32:57 +0530

    added recursive helper function

--- tree.c
+++ tree.c
@@ -130,12 +130,13 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 //   - object_write    : save that binary buffer to the store as OBJ_TREE
 //
 // Returns 0 on success, -1 on error.
+static int build(Index *idx, const char *prefix, ObjectID *out);
+
 int tree_from_index(ObjectID *id_out) {
     Index idx;
 
     if (index_load(&idx) < 0)
         return -1;
 
-    (void)id_out;
-    return 0;
+    return build(&idx, "", id_out);
 }
