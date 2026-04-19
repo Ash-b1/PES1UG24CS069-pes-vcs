5745716a6745eff84bd4d4020941dbe83b18f242
vboxuser <vboxuser@unit-4.myguest.virtualbox.org>
2026-04-19 18:55:53 +0530

    commit_create: read parent commit from HEAD

--- commit.c
+++ commit.c
@@ -198,8 +198,10 @@ int head_update(const ObjectID *new_commit) {
 int commit_create(const char *message, ObjectID *commit_id_out) {
     ObjectID tree_id;
 
-    // Step 1: build tree from index
     if (tree_from_index(&tree_id) != 0) return -1;
+
+    ObjectID parent_id;
+    int has_parent = (head_read(&parent_id) == 0);
 
     (void)message; (void)commit_id_out;
     return 0;
